76d97cef2eb4554302b50700d3addefdf7bc8606
oapache <160090075+oapache@users.noreply.github.com>
2025-05-23 10:44:59 -0300

    Desafio: Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-void imprimeTabuleiro(int tabuleiro[10][10]) {
+#define LINHAS 10 
+#define COLUNAS 10
+
+void imprimeTabuleiro(int tabuleiro[LINHAS][COLUNAS]) {
     char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int linhas[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    printf("   ");
-    for (int i = 0; i < 10; i++) {
-        printf(" %c", colunas[i]);
-    }
-    printf("\n");
-
-    for (int i = 0; i < 10; i++) {
-        printf("%2d ", linhas[i]);
-        for (int j = 0; j < 10; j++) {
-            printf(" %d", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
+@@ -20,23 +23,45 @@ void imprimeTabuleiro(int tabuleiro[10][10]) {
     return;
 };
 
-void posicionaNavioHorizontal(int tabuleiro[10][10], int posicaoColuna, int posicaoLinha) {
+void posicionaNavioHorizontal(int tabuleiro[LINHAS][COLUNAS], int posicaoColuna, int posicaoLinha) {
     for (int i = posicaoColuna; i < posicaoColuna + 3; i++) {
         tabuleiro[posicaoLinha][i] = 3;
     }
     return;
 }
 
-void posicionaNavioVertical(int tabuleiro[10][10], int posicaoColuna, int posicaoLinha) {
+void posicionaNavioVertical(int tabuleiro[LINHAS][COLUNAS], int posicaoColuna, int posicaoLinha) {
     for (int i = posicaoLinha; i < posicaoLinha + 3; i++) {
         tabuleiro[i][posicaoColuna] = 3;
     }
@@ -35,20 +26,39 @@ void posicionaNavioVertical(int tabuleiro[10][10], int posicaoColuna, int posica
     return;
 }
 
+void posicionaNavioDiagonalDireita(int tabuleiro[LINHAS][COLUNAS], int posicaoColuna, int posicaoLinha) {
+    int controle = 0;
+    for (int i = posicaoLinha; i < posicaoLinha + 3; i++) {
+        tabuleiro[posicaoLinha - controle][posicaoColuna + controle] = 3;
+        controle++;
+    }
+    
+    return;
+
+};
+
+void posicionaNavioDiagonalEsquerda(int tabuleiro[LINHAS][COLUNAS], int posicaoColuna, int posicaoLinha) {
+    int controle = 0;
+    for (int i = posicaoLinha; i < posicaoLinha + 3; i++) {
+        tabuleiro[posicaoLinha + controle][posicaoColuna + controle] = 3;
+        controle++;
+    }
+    
+    return;
+
+};
+
 int main() {
-    int tabuleiro[10][10];
+    int tabuleiro[LINHAS][COLUNAS];
 
     for(int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
-        }
-    }
-
-    printf("Batalha Naval\n");
-    printf("Tabuleiro:\n");
+@@ -49,6 +74,8 @@ int main() {
 
     posicionaNavioHorizontal(tabuleiro, 2, 3);
     posicionaNavioVertical(tabuleiro, 3, 5);
+    posicionaNavioDiagonalDireita(tabuleiro, 7, 3);
+    posicionaNavioDiagonalEsquerda(tabuleiro, 5, 7);
 
     imprimeTabuleiro(tabuleiro);
 
